933ed93c56920f7696e600a953fe8402ec57692e
Joao Luiz de Figueiredo Magalhães <dukejlf@gmail.com>
2023-11-13 14:40:32 -0300

    fixed the calendar, added responsive size.

--- src/components/CalendarView.vue
+++ src/components/CalendarView.vue
@@ -1,49 +1,55 @@
 <template>
-  <div class="flex flex-col w-[1050px]">
-    <div class="flex w-full">
-      <div class="text-center w-[150px] bg-primary text-white border" v-for="day in daysOfWeek" :key="day">{{ day }}</div>
+  <div class="flex flex-col max-w-[700px] w-full px-5">
+    <div class="grid grid-cols-7 w-full">
+      <div class="flex justify-center bg-primary text-white border" v-for="day in daysOfWeek" :key="day">
+        <span class="hidden sm:!flex ">{{ day }}</span>
+        <span class="sm:hidden">{{ day.substring(0, 3) }}</span>
+      </div>
     </div>
-    <div class="flex flex-wrap" v-for="week in daysInMonth" :key="week[index]">
-      <div class="flex-1 text-center border p-4" v-for="day in week" :key="day[index]">
+    <div class="grid grid-cols-7" v-for="(week, weekIndex) in daysInMonth" :key="weekIndex">
+      <div class="w-full h-20 border p-2" v-for="(day, dayIndex) in week" :key="dayIndex">
         {{ day.day }}
       </div>
     </div>
   </div>
 </template>
 
-<script setup>
-import { ref, onMounted } from 'vue'
-import { getMonth, getYear, startOfMonth, endOfMonth, eachDayOfInterval, startOfWeek, endOfWeek, format } from 'date-fns'
 
-const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-const currentDate = new Date()
-const month = getMonth(currentDate)
-const year = getYear(currentDate)
-const daysInMonth = ref([])
+<script setup>
+import { ref, onMounted } from 'vue';
+import { getMonth, getYear, startOfMonth, endOfMonth, eachDayOfInterval, startOfWeek, endOfWeek, format } from 'date-fns';
+
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const currentDate = new Date();
+const month = getMonth(currentDate);
+const year = getYear(currentDate);
+const daysInMonth = ref([]);
 
 onMounted(() => {
-  calculateDaysInMonth()
-})
+  calculateDaysInMonth();
+});
 
 function calculateDaysInMonth() {
+  const start = startOfMonth(new Date(year, month));
+  const end = endOfMonth(new Date(year, month));
+  const startWeek = startOfWeek(start);
+  const endWeek = endOfWeek(end);
+  const days = eachDayOfInterval({ start: startWeek, end: endWeek });
 
-  const start = startOfMonth(new Date(year, month))
-  const end = endOfMonth(new Date(year, month))
-  const days = eachDayOfInterval({ start, end })
-
-  let week = []
+  let week = [];
   days.forEach(day => {
-    if (week.length > 0 && day.getDay() === 0) {
-      daysInMonth.value.push(week)
-      week = []
+    if (week.length && day.getDay() === 0) {
+      daysInMonth.value.push(week);
+      week = [];
     }
     week.push({
       date: format(day, 'yyyy-MM-dd'),
-      day: format(day, 'd')
-    })
-  })
-  if (week.length > 0) {
-    daysInMonth.value.push(week)
+      day: day >= start && day <= end ? format(day, 'd') : ''
+    });
+  });
+  if (week.length) {
+    daysInMonth.value.push(week);
   }
 }
 </script>
+
